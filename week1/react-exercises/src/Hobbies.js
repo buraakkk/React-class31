307fd85648b6a0fb3e547e6bd4f8dcd76c9b775b
Burak <isikburak142@gmail.com>
2021-05-18 11:48:19 +0200

    First exercise is done after watching read-me video.

--- week1/react-exercises/src/Hobbies.js
+++ week1/react-exercises/src/Hobbies.js
@@ -1,6 +1,36 @@
-function Hobbies(props) {
-  const element = props.element;
-  return <li>{element}</li>;
+import React from "react";
+
+function Hobby({ hobby }) {
+  //Is it a functional component  ?
+  return (
+    <>
+      <li>{hobby}</li>
+    </>
+  );
 }
 
-export default Hobbies;
+function HobbyList() {
+  const title = "My Hobby List" ;
+  const hobbies = [
+    "Surfing",
+    "Rock climbing",
+    "Mountain biking",
+    "Breakdancing",
+  ];
+
+  return (
+    <div>
+      <div>
+        <h1>{title}</h1>
+      </div>
+
+      <div>
+        {hobbies.map((hobby, index) => {
+          return <Hobby key={index} hobby={hobby} />; // Is that line same with Hobby(hobby) ?
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default HobbyList;
