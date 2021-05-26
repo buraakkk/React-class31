53e84a92b64e7c0bc2e9ce918a4eac0ebfbe677e
Burak <isikburak142@gmail.com>
2021-05-26 12:57:52 +0200

    DogGallery component is added into App.js

--- week2/react-exercises/src/App.js
+++ week2/react-exercises/src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./App.css";
 import Friend from "./ex1/friends";
+import DogGallery from "./ex2/DogGallery";
 
 
 function App() {
   return (
     <>
       <Friend />
-
+      <DogGallery />
     </>
   );
 }
