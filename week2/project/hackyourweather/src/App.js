72a1ba45ae99dd5ae911f45d754398b56970cade
Burak <isikburak142@gmail.com>
2021-05-29 02:44:00 +0200

    call the first function

---
+++ week2/project/hackyourweather/src/App.js
@@ -0,0 +1,13 @@
+import Weather from "./Weather";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Weather</h1>
+      <Weather />
+    </div>
+  );
+}
+
+export default App;
